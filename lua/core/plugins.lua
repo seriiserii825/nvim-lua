5a806682f978257ad87c95b9e94af370067354d6
serii burduja <seriiburduja@gmail.com>
2025-02-02 18:03:47 +0200

    feat: theme

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -57,4 +57,12 @@ require("lazy").setup({
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
+{
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require('nordic').load()
+    end
+}
 });
