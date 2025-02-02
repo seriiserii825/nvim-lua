38c723c278a65fa881c9bdb9a77773498231a7f5
serii burduja <seriiburduja@gmail.com>
2025-02-02 19:52:15 +0200

    feat: format lua with style lua

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,85 +1,88 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
-		"git", "clone", "--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-		lazypath
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
 	})
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	{
-  'nvim-telescope/telescope.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  version = false -- This installs the latest stable version
-},
-{
-  "nvim-tree/nvim-tree.lua",
-  version = "*", -- you can specify a version if needed
-  lazy = false,  -- we want it loaded immediately
-  dependencies = {
-    "nvim-tree/nvim-web-devicons", -- required for file icons
-  },
-  config = function()
-    require("nvim-tree").setup({
-      -- you can add custom configuration here
-    })
-  end,
-},
-    {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  keys = {
-   {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
-  },
-},
-{'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
-      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-    },
-    build = "make tiktoken", -- Only on MacOS or Linux
-    opts = {
-      -- See Configuration section for options
-    },
-    -- See Commands section for default commands if you want to lazy load on them
-  },
-{
-    'AlexvZyl/nordic.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require('nordic').load()
-    end
-},
-{'sheerun/vim-polyglot'},
-{'vim-airline/vim-airline'},
-{'preservim/vim-indent-guides'},
-  -- amongst your other plugins
-  -- {'akinsho/toggleterm.nvim', version = "*", opts = {direction =  'float',}}
-  {'voldikss/vim-floaterm'},
-  {'tpope/vim-surround'},
-  {'jiangmiao/auto-pairs'},
-  {'tomtom/tcomment_vim'},
-  {'mg979/vim-visual-multi'},
-{
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  ---@type Flash.Config
-  opts = {},
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		version = false, -- This installs the latest stable version
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*", -- you can specify a version if needed
+		lazy = false, -- we want it loaded immediately
+		dependencies = {
+			"nvim-tree/nvim-web-devicons", -- required for file icons
+		},
+		config = function()
+			require("nvim-tree").setup({
+				-- you can add custom configuration here
+			})
+		end,
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+	{ "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	},
+	{
+		"AlexvZyl/nordic.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("nordic").load()
+		end,
+	},
+	{ "sheerun/vim-polyglot" },
+	{ "vim-airline/vim-airline" },
+	{ "preservim/vim-indent-guides" },
+	-- amongst your other plugins
+	-- {'akinsho/toggleterm.nvim', version = "*", opts = {direction =  'float',}}
+	{ "voldikss/vim-floaterm" },
+	{ "tpope/vim-surround" },
+	{ "jiangmiao/auto-pairs" },
+	{ "tomtom/tcomment_vim" },
+	{ "mg979/vim-visual-multi" },
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
@@ -88,19 +91,34 @@ require("lazy").setup({
     -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
-},
-  {'tpope/vim-fugitive'},
-  {'airblade/vim-gitgutter'},
-  {'NvChad/nvim-colorizer.lua'},
-  {'SirVer/ultisnips'},
-  {'honza/vim-snippets'},
- {'hrsh7th/cmp-nvim-lsp'},
- {'hrsh7th/cmp-buffer'},
- {'hrsh7th/cmp-path'},
-    {'hrsh7th/cmp-cmdline'},
-    {'hrsh7th/nvim-cmp'},
-    {'lewis6991/gitsigns.nvim'},
-  {'neovim/nvim-lspconfig'},
-  { "williamboman/mason.nvim"},
-  {"nvimtools/none-ls.nvim"}
-});
+	},
+	{ "tpope/vim-fugitive" },
+	{ "airblade/vim-gitgutter" },
+	{ "NvChad/nvim-colorizer.lua" },
+	{ "SirVer/ultisnips" },
+	{ "honza/vim-snippets" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/cmp-cmdline" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "lewis6991/gitsigns.nvim" },
+	{ "neovim/nvim-lspconfig" },
+	{ "williamboman/mason.nvim" },
+	{ "nvimtools/none-ls.nvim" },
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+	},
+})
