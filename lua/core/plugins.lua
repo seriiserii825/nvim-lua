a39f6425d0fe9a84cc914ce54cbb56ab5d88898e
serii burduja <seriiburduja@gmail.com>
2025-02-02 18:02:23 +0200

    feat: copilot chat

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -13,7 +13,19 @@ require("lazy").setup({
   dependencies = { 'nvim-lua/plenary.nvim' },
   version = false -- This installs the latest stable version
 },
-{"nvim-tree/nvim-tree.lua"},
+{
+  "nvim-tree/nvim-tree.lua",
+  version = "*", -- you can specify a version if needed
+  lazy = false,  -- we want it loaded immediately
+  dependencies = {
+    "nvim-tree/nvim-web-devicons", -- required for file icons
+  },
+  config = function()
+    require("nvim-tree").setup({
+      -- you can add custom configuration here
+    })
+  end,
+},
     {
   "folke/which-key.nvim",
   event = "VeryLazy",
@@ -33,7 +45,16 @@ require("lazy").setup({
   },
 },
 {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
-{"CopilotC-Nvim/CopilotChat.nvim"},
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
       { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 });
