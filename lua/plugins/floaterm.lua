f4f22e8d7414f6e67677ea343edb3a9d664492fd
serii burduja <seriiburduja@gmail.com>
2025-02-03 11:03:55 +0200

    feat: floatterm

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -3,3 +3,4 @@ vim.g.floaterm_wintype = 'float'
 vim.g.floaterm_width = 0.96
 vim.g.floaterm_height = 0.96
 vim.api.nvim_set_keymap('n', '<C-b>', ':FloatermToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<C-b>', '<C-\\><C-n>:FloatermToggle<CR>', { noremap = true, silent = true })
