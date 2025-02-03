158c8166bed07c09360b9e317226688735456341
serii burduja <seriiburduja@gmail.com>
2025-02-03 10:47:14 +0200

    feat: split find files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,11 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+
+-- Add the new key mapping for <leader>fs
+vim.keymap.set("n", "<leader>fs", function()
+  vim.cmd("vsplit")
+  builtin.find_files()
+end, { desc = "Telescope find files in vertical split" })
